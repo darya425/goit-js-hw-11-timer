47bbb29df4108a08cccd5004bece9c8a763cdbda
darya425 <dasha425@mail.ru>
2021-05-04 15:00:03 +0200

    fixed date

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ class CountdownTimer {
 
 const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019')
+    targetDate: new Date('Jul 17, 2021')
 });
 
 window.addEventListener('load', timer.start.bind(timer));
